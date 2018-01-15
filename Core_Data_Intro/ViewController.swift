a42632351b9a0f8cda1b60c827134f90c6b81ae4
Charlie <charliecuddy4@gmail.com>
2018-01-15 15:10:54 -0600

    put a fetch request saved core data permanently

--- Core_Data_Intro/ViewController.swift
+++ Core_Data_Intro/ViewController.swift
@@ -7,12 +7,69 @@
 //
 
 import UIKit
+import CoreData
+
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let newUser = NSEntityDescription.insertNewObject(forEntityName: "Users", into: context)
+        
+        newUser.setValue("Josie", forKey: "username")
+        newUser.setValue("myPassword", forKey: "password")
+        newUser.setValue(30, forKey: "age")
+        
+        do {
+            
+            try context.save()
+            print("Saved")
+            
+        } catch {
+            
+            
+            print("There was an error")
+            
+        }
+        
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Users")
+        
+        request.returnsObjectsAsFaults = false
+        
+        do {
+            
+            let results = try context.fetch(request)
+            
+            if results.count > 0 {
+                
+                for result in results as! [NSManagedObject] {
+                
+                    if let username = result.value(forKey: "username") as? String {
+                    
+                        print(username)
+                    
+                    }
+                    
+                }
+                
+            } else {
+                
+                print("No results")
+                
+            }
+            
+        } catch {
+            
+            print("Couldn't fetch results")
+            
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
